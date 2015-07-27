668df22b5c0ba13f98f50c5d03ea587b7248d2a8
Michael Fich <michaeltf@gmail.com>
2015-07-27 15:22:15 -0400

    Added user registration and basic structure/style to the app layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
   has_many :strategies
+
+  validates :password, length: { minimum: 4 }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
