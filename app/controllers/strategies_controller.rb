16ec25317ded011c7e5252a3a8f94f038b8159ef
Michael Fich <michaeltf@gmail.com>
2015-07-28 16:22:46 -0400

    Updated the Strategy controller and views to include methods for edit, update, and show

--- app/controllers/strategies_controller.rb
+++ app/controllers/strategies_controller.rb
@@ -8,12 +8,7 @@ class StrategiesController < ApplicationController
   def new
     @strategy = Strategy.new
     @strategy.indicators.build
-    counter = 0;
-    @comparisons = Indicator::COMPARATORS.inject([]) do |result, value|
-      result.push([value, counter])
-      counter += 1;
-      result
-    end
+    @comparisons = get_comparators
   end
 
   def create
@@ -26,6 +21,26 @@ class StrategiesController < ApplicationController
     end
   end
 
+  def show
+    @strategy = Strategy.find(params[:id])
+  end
+
+  def edit
+    @strategy = Strategy.find(params[:id])
+    @comparisons = get_comparators
+  end
+
+  def update
+    @strategy = Strategy.find(params[:id])
+    @strategy.update(strategy_params)
+    if @strategy.save
+      flash[:notice] = "Successfully updated your #{@strategy.name} investment strategy."
+      redirect_to @strategy
+    else
+      render :edit
+    end
+  end
+
   private
   def strategy_params
     params
@@ -34,5 +49,14 @@ class StrategiesController < ApplicationController
               indicators_attributes: [
                 :id, :name, :value, :comparison, :period, :action, :_destroy
               ])
+  end
+
+  def get_comparators
+    counter = 0;
+    Indicator::COMPARATORS.inject([]) do |result, value|
+      result.push([value, counter])
+      counter += 1;
+      result
+    end
   end
 end
