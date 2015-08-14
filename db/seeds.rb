adf93df09706a3eeb95a6ce682eeb986983a388d
Michael Fich <michaeltf@gmail.com>
2015-08-14 18:10:47 -0400

    Fixed seeds.rb for Heroku deployment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
-require 'open-uri'
-require 'json'
+# require 'open-uri'
+# require 'json'
 
-def formatDate(date)
-  return date.strftime("%Y-%m-%d")
-end
+# def formatDate(date)
+#   return date.strftime("%Y-%m-%d")
+# end
 
-end_date = Date.today
-start_date = end_date - 3.years
+# end_date = Date.today
+# start_date = end_date - 3.years
 
-api = "http://api.coindesk.com/v1/bpi/historical/close.json?start=#{start_date}&end=#{end_date}"
+# api = "http://api.coindesk.com/v1/bpi/historical/close.json?start=#{start_date}&end=#{end_date}"
 
-data = JSON.parse( open(api).read )
-data = data["bpi"]
+# data = JSON.parse( open(api).read )
+# data = data["bpi"]
 
-Tick.destroy_all
+# Tick.destroy_all
 
-data.each do |date, value|
-  Tick.create(datetime: date, last_price: value)
-end
+# data.each do |date, value|
+#   Tick.create(datetime: date, last_price: value)
+# end
 
 if Formula.count < 1
   Formula.create(
