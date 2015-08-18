ef9d65e3e92990c13dc5d97096d133c16151b6ff
Michael Fich <michaeltf@gmail.com>
2015-08-17 22:51:11 -0400

    Seeds file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
+Formula.destroy_all
 Formula.create(name: "sma", abbreviation: "SMA", full_name: "Simple Moving Average", order_number: 1)
 Formula.create(name: "ema", abbreviation: "EMA", full_name: "Exponential Moving Average", order_number: 2)
 Formula.create(name: "macd", abbreviation: "MACD", full_name: "Moving Average Convergence Divergence", order_number: 3)
 Formula.create(name: "rsi", abbreviation: "RSI", full_name: "Relative Strength Index", order_number: 4)
 
+User.destroy_all
 User.create(
   first_name: "michael",
   last_name: "fich",
